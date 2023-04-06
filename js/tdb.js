5430947b56708c35916f24b6ac91b7b49036acf8
Yann Langlais <ylanglais@gmail.com>
2023-04-06 18:21:45 +0200

    add spinner hourglass

--- js/tdb.js
+++ js/tdb.js
@@ -8,6 +8,7 @@ function tdb_form(module, titre) {
 	$("#data_area").load("tdb_form.php", {'module': module, 'titre': titre});
 }
 function tdb_rpt(rptname) {
+	document.getElementById("data_area").innerHTML = '<div id="a_' + rptname + '" style=" position:absolute;top: 0px; left:0px;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"><div style="position:absolute; top: 45%;width:100%;text-align: center; vertical-align: middle;color: white;"><img src="images/wait.gif" width="100px"/><br/>Computing...</div></div>';
 	$("#data_area").load("tdb_rpt.php", {'rptname': rptname});
 }
 function tdb_page(page) {
