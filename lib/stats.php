734d373eeed8f8b93f945b166fe51e4af40e42bb
Yann Langlais <ylanglais@gmail.com>
2023-04-07 18:28:41 +0200

    fix typo

--- lib/stats.php
+++ lib/stats.php
@@ -11,14 +11,12 @@ function stats_get($key) {
 function stats_update($key, $val) {
 	$o = stats_get($key);
 	if ($o === false) {
-		dbg(">> insert into stats.duration values ('$key', 1, $val, $val, $val)");
 		$q = new query("insert into stats.duration values ('$key', 1, $val, $val, $val)");
 	} else {
 		if ($val < $o->min) $o->min = $val;
 		if ($val > $o->max) $o->max = $val;
-		$o->avg = ($n * $o->avg + $val) / ($n + 1);
+		$o->avg = ($o->n * $o->avg + $val) / ($n + 1);
 		$o->n++;
-		dbg(">> update stats.duration set n = $o->n, min = $o->min, max = $o->max, avg = $o->avg where key = '$key'");
 		new query("update stats.duration set n = $o->n, min = $o->min, max = $o->max, avg = $o->avg where key = '$key'");
 	}
 }
