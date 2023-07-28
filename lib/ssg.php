de8a315f118296d18dde891eb5d8a36963e3573a
Yann LANGLAIS <ylanglais@gmail.com>
2023-07-28 11:19:20 +0200

    add ssh component

---
+++ lib/ssg.php
@@ -0,0 +1,19 @@
+<?php
+function ssg($host, $cmd) {
+	$l = gethostname();
+	$out = [];
+	$xit = 0;
+	exec("$bin $host '$cmd'", $out, $xit);
+	if ($xit != 0 && count($out) == 0) return false;
+	return $out;
+}
+function scpg($host, $src, $dst) {
+	$l = gethostname();
+	$bin = "scp";
+	$xit = 0;
+	$out = [];
+	exec("$bin $host:$src $dst", $out, $xit); 
+	if (file_exists($dst)) return true;
+	return false;
+}
+?>
