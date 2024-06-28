3592b52371311884f06e4b68b8fe721675d01459
Yann Langlais <ylanglais@gmail.com>
2024-06-28 10:19:31 +0200

    fix return when no element id given

--- js/ctrl.js
+++ js/ctrl.js
@@ -1,12 +1,13 @@
-function ctrl(ctrl, data, id = null,  sync = true) {
+function ctrl(ctrl, data, id = null, sync = true) {
 	data.ctrl = ctrl; 
 
 	if (id == null) {
-		sync_post("ctrl.php", data);
-	} else if (sync == false) {
+		return sync_post("ctrl.php", data);
+	}
+	if (sync == false) {
 		async_load(id, "ctrl.php", data);
 	} else {
 		load(id, "ctrl.php", data);
 	}
-	
+	return true;	
 }
