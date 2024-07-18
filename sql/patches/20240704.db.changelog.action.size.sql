a6d29f4fc2f6b027ba81d754a5c5b8693988530c
Yann Langlais <ylanglais@gmail.com>
2024-07-18 18:44:16 +0200

    fix

--- sql/patches/20240704.db.changelog.action.size.sql
+++ sql/patches/20240704.db.changelog.action.size.sql
@@ -1,2 +1,2 @@
 ALTER TABLE db.changelog alter column action type varchar(200);
-insert into db.changelog (action) values ('patch 20240704.param.page.unique.name.sql');
+insert into db.changelog (action) values ('patch 20240704.db.changelog.action.size.sql');
