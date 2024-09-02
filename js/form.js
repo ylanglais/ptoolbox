24eb536c64060c38ea155f9ed10ef69b995d63a8
Yann Langlais <ylanglais@gmail.com>
2024-09-02 18:50:34 +0200

    fix form parameters reading

--- js/form.js
+++ js/form.js
@@ -1,16 +1,11 @@
-function form_load(name, form, action) {
- 	form = atob(form);
-	form = JSON.parse(form);
-	let data = {};
-	data["fname"] = form.fname;
-	data["form"]   = true;
-	data["action"] = action;
-	for (i in form.params) {
-		if (form.params[i].type == 'date') {
+function parse_params(data, params) {
+	for (i in params) {
+		//console.log(i);
+		if (params[i].type == 'date') {
 			var v = document.getElementById(i).value;
 			var val = v.substring(6,10) + "-" + v.substring(3,5) + "-" + v.substring(0,2);
 			data[i] = val;
-		} else if (form.params[i].type == 'list' || form.params[i].type == 'mlist') {
+		} else if (params[i].type == 'list' || params[i].type == 'mlist') {
 			var r = [];
 			var v = document.getElementById(i);
 			//console.log(i + " + " + JSON.stringify(v));
@@ -26,7 +21,24 @@ function form_load(name, form, action) {
 		} else {
 			data[i] = document.getElementById(i).value;
 		}
-	}	
+		//console.log("data[" + i + "]: " + data[i]);
+	}
+}
+function form_load(name, form, action) {
+ 	form = atob(form);
+	form = JSON.parse(form);
+	let data = {};
+	data["fname"] = form.fname;
+	data["form"]   = true;
+	data["action"] = action;
+
+	if (form.param_groups != []) {
+		for (j in form.param_groups) 
+			parse_params(data, form.param_groups[j]);
+	}
+
+	//console.log(data);
+	
 	progress('form_result', form.fname);
 	ctrl("form", data, 'form_result');
 }
